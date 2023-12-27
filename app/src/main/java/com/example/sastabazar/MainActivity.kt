c517e4b015eb46ce6f310158d7c140e83c8b7da0
Rajat Sharma <rajat200229@gmail.com>
2023-12-27 23:15:01 +0530

    SplashScreen Added

--- app/src/main/java/com/example/sastabazar/MainActivity.kt
+++ app/src/main/java/com/example/sastabazar/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.sastabazar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -12,20 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun GotoNext(view: View) {
-        if(FirebaseAuth.getInstance().currentUser == null)
-        {
-            startActivity(Intent(this,SignUpLoginActivity::class.java))
-            finish()
-        }
-        else
-        {
-            startActivity(Intent(this,HomeActivity ::class.java))
-            finish()
-        }
-
 
     }
 }
---
+++ app/src/main/java/com/example/sastabazar/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.sastabazar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(FirebaseAuth.getInstance().currentUser == null)
+            {
+                startActivity(Intent(this,SignUpLoginActivity::class.java))
+                finish()
+            }
+            else
+            {
+                startActivity(Intent(this,HomeActivity ::class.java))
+                finish()
+            }
+        },3000)
+
+    }
+}
